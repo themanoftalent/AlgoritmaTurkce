2a9c605fb106e311ccffcd8615284573e6520e6a
İbrahim Koç <112804288+iibrahimkoc@users.noreply.github.com>
2024-03-06 11:23:30 +0300

    Add files via upload

---
+++ JavaPractices/SayiToplam.java
@@ -0,0 +1,13 @@
+// ibrahim koç 2211502004
+class SayiToplam {
+    public static void main(String[] args){
+        
+        int num1 = 4;
+        int num2 = 3;
+        int sum = num1 + num2;
+        
+        System.out.println("Sayıların Toplamı:" + sum);
+        
+    }
+}
+
