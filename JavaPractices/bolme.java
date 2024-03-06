60c4a2cf6bb319f761362bc443a2443ece778bdd
Çağrı Demirkıran <162141586+Dmrkrn@users.noreply.github.com>
2024-03-06 11:37:48 +0300

    cagridemirkiran2211502023
    Çağrı Demirkıran 2211502023

---
+++ JavaPractices/bolme.java
@@ -0,0 +1,10 @@
+public class bolme {
+    public static void main(String[] args) {
+        int sayi1 = 5;
+        double sayi2 = 27;
+
+        double bolmeSonucu = sayi1 / sayi2;
+
+        System.out.println("iki sayının bölümü:"+bolmeSonucu);
+    }
+}
