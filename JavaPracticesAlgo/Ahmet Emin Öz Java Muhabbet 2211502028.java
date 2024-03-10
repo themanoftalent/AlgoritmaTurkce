8aa98af89c3351f994c713caf2403bbd63ce0233
Ahmet Emin Öz <161753302+1alexdegil@users.noreply.github.com>
2024-03-11 02:16:36 +0300

    Create Ahmet Emin Öz Java Muhabbet 2211502028.java

---
+++ JavaPracticesAlgo/Ahmet Emin Öz Java Muhabbet 2211502028.java
@@ -0,0 +1,8 @@
+public class Sayibolme{
+    public static void main(String[] args){
+        String msg1= "Selam Ahmet!";
+        String msg2= "Selam Bilal!";
+        System.out.println(msg1);
+        System.out.println(msg2);
+    }
+}
