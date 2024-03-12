536f23fef418102a09af8cb54712bae742f6aec3
YigitEfeBasaran <161735427+YigitEfeBasaran@users.noreply.github.com>
2024-03-12 18:12:01 +0300

    Update yigitefebasaran2211502049(iki sayının çarpımı).java

--- AlgoÖdevler/Ödev_hafta3/yigitefebasaran2211502049(iki sayının çarpımı).java
+++ AlgoÖdevler/Ödev_hafta3/yigitefebasaran2211502049(iki sayının çarpımı).java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 public class ikisayinincarpimi {
     public static void main(String[]args){
         Scanner okuyucu = new Scanner(System.in);
-        System.out.print("please enter first number:");
+        System.out.print("please enter the first number:");
         int number1 = okuyucu.nextInt();
-        System.out.print("please enter second number:");
+        System.out.print("please enter the second number:");
         int number2 = okuyucu.nextInt();
 
         int carpim = number1 * number2;
